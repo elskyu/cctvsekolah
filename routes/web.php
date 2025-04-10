bc90b614cec5451052126cc7c27a29b1a8c1e57d
FandiCahya <fandicahya56@gmail.com>
2025-04-10 14:11:01 +0700

    Update:Bug Navbar dan add Data Users

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\sekolahController;
 
-Route::get('/uji', function () {
+Route::get('/dashboard', function () {
     return view('admin.dashboard');
-});
+})->name('dashboard');
 
 // Route sekolah
 Route::get('/', [sekolahController::class, 'dashboard'])->name('sekolah.sekolah');
