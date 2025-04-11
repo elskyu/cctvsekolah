01aeb521e0daecb0f0290024c550b6a790391fe6
Tempebenguk <erricksb592@gmail.com>
2025-04-11 11:34:26 +0700

    Add : Sanctum Authentication

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiSekolahController;
 use App\Http\Controllers\Api\ApiUsersController;
+use App\Http\Controllers\Api\ApiAuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// ROUTE LOGIN
+Route::post('/login', [ApiAuthController::class, 'login']);
 
-Route::apiResource('/cctv-sekolah', ApiSekolahController::class);
-Route::apiResource('/users', ApiUsersController::class);
+// API CCTV SEKOLAH (GUEST)
+Route::get('/cctv-sekolah', [ApiSekolahController::class, 'index']);
+Route::get('/cctv-sekolah/{id}', [ApiSekolahController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // USER INFO
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // LOGOUT
+    Route::post('/logout', [ApiAuthController::class, 'logout']);
+
+    // API CCTV SEKOLAH
+    Route::post('/cctv-sekolah', [ApiSekolahController::class, 'store']);
+    Route::put('/cctv-sekolah/{id}', [ApiSekolahController::class, 'update']);
+    Route::delete('/cctv-sekolah/{id}', [ApiSekolahController::class, 'destroy']);
+
+    // API USERS
+    Route::apiResource('/users', ApiUsersController::class);
+});
