bb535827bc067a304973d65e76e4304cf22365b1
elskyu <aditiyapratama2014@gmail.com>
2025-04-17 09:37:59 +0700

    Update: logo navbar & styling mini sidebar

--- resources/views/admin/components/nav-logo.blade.php
+++ resources/views/admin/components/nav-logo.blade.php
@@ -1,5 +1,6 @@
 <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-<a class="navbar-brand brand-logo mr-5" href="#"><img src="{{ asset('skydash/images/lifemedia_logo.png') }}" class="mr-2"
-        alt="logo" /></a>
-<a class="navbar-brand brand-logo-mini" href="#"><img src="{{ asset('skydash/images/logo-mini.svg') }}" alt="logo" /></a>
+    <a class="navbar-brand brand-logo mr-5" href="#"><img src="{{ asset('skydash/images/lifemedia_logo.png') }}"
+            class="mr-2" alt="logo" /></a>
+    <a class="navbar-brand brand-logo-mini" href="#"><img src="{{ asset('skydash/images/lifemedia_logo2.png') }}"
+            alt="logo" style="width: 35px; height: 35px;" /></a>
 </div>
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,16 +1,28 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
+    <style>
+        .menu-icon-wrapper {
+            width: 24px;
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+
     <ul class="nav">
         <li class="nav-item">
             <a class="nav-link" href="{{ route('dashboard') }}">
-                <i class="fa-solid fa-house menu-icon" style="font-size: 9pt;"></i>
+                <div class="menu-icon-wrapper">
+                    <i class="fa-solid fa-house menu-icon" style="font-size: 12pt;"></i>
+                </div>
                 <span class="menu-title">Dashboard</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <i class="fa-solid fa-user menu-icon" style="font-size: 11pt;"></i>
+                <div class="menu-icon-wrapper">
+                    <i class="fa-solid fa-user menu-icon" style="font-size: 12pt;"></i>
+                </div>
                 <span class="menu-title">Kelola Pengguna</span>
                 <i class="menu-arrow"></i>
             </a>
@@ -24,7 +36,9 @@
         <li class="nav-item">
             <a class="nav-link {{ request()->is('sekolah*') || request()->is('panorama*') ? '' : 'collapsed' }}"
                 data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-                <i class="fas fa-bookmark menu-icon" style="font-size: 12pt;"></i>
+                <div class="menu-icon-wrapper">
+                    <i class="fas fa-bookmark menu-icon" style="font-size: 12pt;"></i>
+                </div>
                 <span class="menu-title">Kelola Data</span>
                 <i class="menu-arrow"></i>
             </a>
