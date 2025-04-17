b8bad44c26a5b3739e34dfdddf7a4212932c9ddb
Hasan Basri <basricungkring39@gmail.com>
2025-04-17 09:45:27 +0700

    update footer

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="footer">
-    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2025
-        <a href="#" target="_blank">Life Media</a> from Yogyakarta</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made
-            with <i class="ti-heart text-danger ml-1"></i></span>
+    <div class="d-flex justify-content-center">
+        <span class="text-muted text-center d-block">Copyright © 2025
+            <a href="#" target="_blank">Life Media</a> from Yogyakarta
+        </span>
     </div>
-    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Developed by <a
-                href="https://www.themewagon.com/" target="_blank">Life Media</a></span>
+    <div class="d-flex justify-content-center">
+        <span class="text-muted text-center d-block">Developed by
+            <a href="https://www.themewagon.com/" target="_blank">Life Media</a>
+        </span>
     </div>
 </footer>
